78156033712d4b27637ad4ac35e8f474ea0005af
shwetaarbune <63236070+shwetaarbune@users.noreply.github.com>
2022-08-18 18:06:35 +0530

    Add files via upload

---
+++ Application.Entities/Entities.cs
@@ -0,0 +1,21 @@
+﻿using Application.Entities.Base;
+namespace Application.Entities;
+
+
+
+public class Department : Entity
+{
+    public int DeptNo { get; set; }
+    public string DeptName { get; set; }
+    public string Location { get; set; }
+    public int Capacity { get; set; }
+}
+
+public class Employee : Entity
+{
+    public int EmpNo { get; set; }
+    public string EmpName { get; set; }
+    public string Designation { get; set; }
+    public int Salary { get; set; }
+    public int DeptNo { get; set; }
+}
